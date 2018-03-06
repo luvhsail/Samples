8ac3db16663231403baa32ad23c36cb4f04d3ec6
bapu <luvshail@yahoo.com>
2018-03-06 10:30:52 +0530

    sample code for windows auth issue in UWP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HttpClientHandler hand = new HttpClientHandler();
+            hand.AllowAutoRedirect = true;
+            hand.UseDefaultCredentials = true;
+            using (var client = new HttpClient(hand))
+            {
+                client.BaseAddress = new Uri(@"http://localhost:57791/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  This returns the JSON containing the current user name as expected. But the same thing fails from a UWP
+
+                HttpResponseMessage response = client.GetAsync("api/values").Result;
+                string name = string.Empty;
+                if (response.IsSuccessStatusCode)
+                {
+                    name = response.Content.ReadAsStringAsync().Result;
+                }
+                Console.WriteLine(name);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ UWPTest/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClientHandler hand = new HttpClientHandler();
+            hand.AllowAutoRedirect = true;
+            hand.UseDefaultCredentials = true;
+            using (var client = new HttpClient(hand))
+            {
+                client.BaseAddress = new Uri(@"http://localhost:57791/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("api/values");                
+                string name = string.Empty;
+                // The same code runs fine from console but fails and returns 500 from UWP
+                if (response.IsSuccessStatusCode)
+                {
+                    name = await response.Content.ReadAsStringAsync();
+                }
+                txtName.Text = name;
+            }
+        }
+    }
+}
